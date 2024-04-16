ca08076a6b41d8afd8f89c132f44ff33f97554e3
Marlon Martins <marlon-martins@outlook.com>
2024-04-16 13:39:49 +0200

    Section 1: Componentes e Props Conclusão da seção

--- 1- Components and Props/section1-app/src/App.js
+++ 1- Components and Props/section1-app/src/App.js
@@ -4,13 +4,6 @@ import HelloWorld from './components/HelloWorld';
 import Profile from './components/Profile';
 import Family from './components/Family';
 import Counter from './components/Counter';
-import Toggle from './components/Toggle';
-import ToggleMultiple from './components/ToggleMultiple';
-import Clock from './components/Clock';
-import TodoList from './components/TodoList';
-import WindowWitdhChecker from './components/WindowWitdhChecker';
-import DataFetcher from './components/DataFetcher';
-
 
 function App() {
 
@@ -43,19 +36,6 @@ function App() {
       <Family members={members}/>
       <h2>5- Component com estado</h2>
       <Counter/>
-      <h1>Section 2:Estado e ciclo de vida</h1>
-      <h2>1- Usando o estado</h2>
-      <Toggle />
-      <h2>2- Toggle Multiplo</h2>
-      <ToggleMultiple/>
-      <h2>3- Relogio em tempo real</h2>
-      <Clock/>
-      <h2>4- Todo List e Filter</h2>
-      <TodoList/>
-      <h2>6- Verificador de largura de tela</h2>
-      <WindowWitdhChecker/>
-      <h2>7- Simulação de busca de dados</h2>
-      <DataFetcher/>
     </div>
   );
 }
