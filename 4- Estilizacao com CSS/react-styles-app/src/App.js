94ec09a3bc141b7910de5e67dfe7d008f01abf18
Marlon Martins <marlon-martins@outlook.com>
2024-06-26 11:41:50 +0200

    55. Ex 20: Estilo com CSS externo

--- 4- Estilizacao com CSS/react-styles-app/src/App.js
+++ 4- Estilizacao com CSS/react-styles-app/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Button from './components/Button';
 import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <h2>Ex 19: HelloWorld com estilos</h2>
-      <HelloWorld/>
+      <HelloWorld />
+      <hr />
+      <h2>Ex 20: Estilo com CSS externo</h2>
+      <Button />
     </div>
   );
 }
---
+++ 4- Estilizacao com CSS/react-styles-app/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import './button.css'
+
+const Button = () => {
+    return (
+        <button className='my-button'>Click me</button>
+    )
+}
+
+export default Button;
