e1304642f755d779423a0939e84b01fa84d824c6
Marlon Martins <marlon-martins@outlook.com>
2024-06-26 10:48:00 +0200

    53. Ex 19: HelloWorld com estilos

--- 4- Estilizacao com CSS/react-styles-app/src/App.js
+++ 4- Estilizacao com CSS/react-styles-app/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
-      
+      <h2>Ex 19: HelloWorld com estilos</h2>
+      <HelloWorld/>
     </div>
   );
 }
---
+++ 4- Estilizacao com CSS/react-styles-app/src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+const HelloWorld = () => {
+  const style = {
+    color: "#DFDFDF",
+    fontSize: "30px",
+    backgroundColor: "#000"
+  }
+  return (
+    <p style={style}>Hello, World</p>
+  )
+}
+
+export default HelloWorld
