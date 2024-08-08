17533d7a6c44502c8df61e49872f8443b676648d
Marlon Martins <marlon-martins@outlook.com>
2024-08-08 11:21:24 +0200

    25. Ex 7: Usando estado

--- 2_Estado e Ciclo de vida/states/src/App.jsx
+++ 2_Estado e Ciclo de vida/states/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Toggle from './components/Toggle'
 
 function App() {
   return (
     <>
-      
+      <h1>Estado e ciclo de vida</h1>
+      <hr />
+      <h2>Ex:7 Usando estado</h2>
+      <Toggle />
+      <hr/>
     </>
   )
 }
---
+++ 2_Estado e Ciclo de vida/states/src/components/Toggle.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+const Toggle = () => {
+    const [isOn, setIsOn] = useState(true);
+  return (
+    <div>
+        <button onClick={() => setIsOn(!isOn)}>{isOn ? "ON" : "OFF"}</button>
+    </div>
+  )
+}
+
+export default Toggle
